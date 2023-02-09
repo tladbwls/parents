903b21ab86ebe54fe7cfb17f4ed8a462258695d5
tladbwls <zipte@naver.com>
2023-02-09 12:50:14 +0900

    event-section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 $(function () {
   $("#fullpage").fullpage({
+    verticalCentered: false,
     autoScrolling: true,
     scrollHorizontally: true,
     navigation: true,
     navigationPosition: "right",
-    paddingTop: "90px",
+    // paddingTop: "90px",
   });
 });
-
-
-
-// active가 들어갔을 때 위에서 아래로 뜨게 
