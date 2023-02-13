4b34ba1cc3d5a3187e08fb38e6a55503944ba7df
tladbwls <zipte@naver.com>
2023-02-13 12:57:12 +0900

    m page start

--- js/main.js
+++ js/main.js
@@ -6,6 +6,32 @@ $(function () {
     navigation: true,
     navigationPosition: "right",
     // paddingTop: "90px",
+    anchors: ["1st", "2st", "3st", "4st", "5st"],
+    afterLoad: function (anchors) {
+      if (anchors == "1st") {
+        // console.log(true);
+        // const fpNavBorder = document.querySelectorAll("#fp-nav ul li");
+        // const fpNavSpan = document.querySelectorAll("#fp-nav ul li a span");
+        $("#fp-nav ul li").css("border", "#fff");
+        $("#fp-nav ul li a span").css("background-color", "#fff");
+        $(".header-wrapper .left .logo-img").css(
+          "background-image",
+          "url(/img/logo-white.png)"
+        );
+      }
+      if (
+        anchors == "2st" ||
+        anchors == "3st" ||
+        anchors == "4st" ||
+        anchors == "5st"
+      ) {
+        $("#fp-nav ul li a span").css("background-color", "#c4c4c4");
+        $(".header-wrapper .left .logo-img").css(
+          "background-image",
+          "url(/img/logo-black.png)"
+        );
+      }
+    },
   });
 });
 
@@ -15,3 +41,9 @@ var swiper = new Swiper(".mySwiper", {
     disableOnInteraction: false,
   },
 });
+
+// function name(params) {}
+
+// const fpNav = document.querySelectorAll("#fp-nav ul li");
+
+// console.log(fpNav);
